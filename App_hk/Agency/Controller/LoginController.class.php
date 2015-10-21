b6ae441506bbd58d3e9288461c81d2322d3ff8ea
jeasonyoung <jeason1914@qq.com>
2015-10-21 16:33:20 +0800

    初始化

---
+++ App_hk/Agency/Controller/LoginController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+
+class LoginController extends Controller{
+	
+	/*用户登陆控制器*/
+	public function login(){
+		$this->display();
+	}
+}
---
+++ App_hk/Common/Conf/config.php
@@ -0,0 +1,34 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'DEFAULT_CHARSET'       =>  'utf-8', // 默认输出编码
+	'URL_MODEL'				=>  2,		 // URL访问模式: 2 采用Rewrite模式 IIS下需配置webconfig文件
+	
+	/* 数据库设置 */
+	'DB_TYPE'               =>  'mysql',     // 数据库类型
+	'DB_HOST'               =>  '127.0.0.1', // 服务器地址
+	'DB_NAME'               =>  'huike',          // 数据库名
+	'DB_USER'               =>  'D9xf1S600K3Op2w',      // 用户名
+	'DB_PWD'                =>  'G8i2e0J5q3v1X7zLn9MM',          // 密码
+	'DB_PORT'               =>  '6033',        // 端口
+	'DB_PREFIX'             =>  'hk_',    // 数据库表前缀
+	'DB_CHARSET'            =>  'utf8',      // 数据库编码默认采用utf8
+	
+	/* SQL解析缓存*/
+	'DB_SQL_BUILD_CACHE'    => true,
+	'DB_SQL_BUILD_QUEUE' => 'xcache',
+	'DB_SQL_BUILD_LENGTH' => 20,
+	
+	/*布局设置*/
+	'TMPL_ENGINE_TYPE'      =>  'Think',     // 默认模板引擎 以下设置仅对使用Think模板引擎有效
+	
+	/*权限认证配置*/
+	'AUTH_CONFIG'			=> array(
+		'AUTH_ON' => true, //认证开关
+		'AUTH_TYPE' => 1, // 认证方式，1为时时认证；2为登录认证
+		'AUTH_GROUP' => 'hk_admin_group', //用户组数据表名
+		'AUTH_GROUP_ACCESS' => 'hk_admin_group_access', //用户组明细表
+		'AUTH_RULE' => 'hk_admin_rule', //权限规则表
+		'AUTH_USER' => 'hk_admin'//用户信息表
+	),
+);
